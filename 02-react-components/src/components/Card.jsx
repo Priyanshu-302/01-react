6b6d597b2a8a57fe88d4813bcc763099946bae77
Priyanshu Das <dasp98458@gmail.com>
2026-03-02 00:40:32 +0530

    Pushing 02-react-components folder to main branch

---
+++ 02-react-components/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className="card">
+      <h1>Hii My name is Priyanshu Das</h1>
+      <h2>My age is 21 years</h2>
+    </div>
+  )
+}
+
+export default Card
